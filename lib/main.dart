88f243c2b7c37f1b3c83a37cdf1de317820bcad3
Masud Rana <MasudRana917775@gmail.com>
2021-10-06 11:31:04 +0600

    apply 4 way of receiving notification

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,22 @@ import 'package:flutter/material.dart';
 import 'package:hello_fire/green_page.dart';
 import 'package:hello_fire/red_page.dart';
 
+// Receive messages when app is in background
+// Here this function is top level. No scope should not restrict it
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print('------------>> backgroundHandler');
+  print(message.data.toString());
+  print(message.notification!.title);
+}
+
 void main() async {
-  // For firebase
+  // Initialize Fireabase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  // It should be top level funtion
+  // It should not in any class
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
 
   runApp(const MyApp());
 }
@@ -43,14 +55,33 @@ class _MyHomePageState extends State<MyHomePage> {
     // TODO: implement initState
     super.initState();
 
-    FirebaseMessaging.instance.getInitialMessage();
-    // Foreground
+    // Gives you the message on which user taps
+    // and it opened the app from terminated state ( like app was closed )
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      if (message != null) {
+        final routeFromMessage = message.data["route"];
+        print('------------>> getInitialMessage');
+        print(routeFromMessage);
+        Navigator.of(context).pushNamed(routeFromMessage);
+      }
+    });
+
+    // Receive messages When the app is in Foreground
     FirebaseMessaging.onMessage.listen((message) {
       if (message.notification != null) {
         print('------------>> Foreground');
         print(message.notification!.body);
         print(message.notification!.title);
       }
+    });
+
+    // Receive messages When the app is in the background but opened and user taps
+    // on the notification
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      final routeFromMessage = message.data["route"];
+      print('------------>> onMessageOpenedApp');
+      print(routeFromMessage);
+      Navigator.of(context).pushNamed(routeFromMessage);
     });
   }
 
